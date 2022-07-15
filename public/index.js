b47ccfb680c9b44ff6234e20b427f581151250fa
Sahil Deshmukh <thesahildeshmukh@gmail.com>
2022-07-14 23:39:30 -0700

    Bouncing animation

--- public/index.js
+++ public/index.js
@@ -1,9 +1,13 @@
 let canvas = document.getElementById("theCanvas");
 let context = canvas.getContext("2d");
-const fps = 20;
 
 context.canvas.width = window.innerWidth - 50;
 context.canvas.height = window.innerHeight - 200;
+
+const baseSpeed = 300;
+const gravityPower = 5;
+
+let start, previousTimeStamp;
 
 const stationaries = [
   {
@@ -23,6 +27,9 @@ const moving = [
     y: (1 / 2) * context.canvas.height,
     radius: 20,
     color: "#d3d3d3",
+    velocity: { x: 1, y: 1 },
+    // mass: 400,
+    index: 0,
   },
 ];
 
@@ -47,14 +54,78 @@ function clearCanvas() {
   context.clearRect(0, 0, context.canvas.width, context.canvas.height);
 }
 
-function updateCanvas() {
-  clearCanvas();
-  drawAll();
-  setTimeout(updateCanvas, 1000 / fps);
+function calculateVelocity(elapsed) {
+  moving.forEach(({ x, y, velocity, index, radius }) => {
+    //#region Bouncing
+    if (
+      y + radius + (velocity.y * elapsed) / 1000 >= context.canvas.height &&
+      velocity.y >= 0
+    ) {
+      moving[index].velocity.y = -1 * moving[index].velocity.y;
+    }
+
+    if (y - radius + (velocity.y * elapsed) / 1000 <= 0 && velocity.y <= 0) {
+      y = radius;
+      moving[index].velocity.y = -moving[index].velocity.y;
+    }
+    if (
+      x + radius + (velocity.x * elapsed) / 1000 >= context.canvas.width &&
+      velocity.x >= 0
+    ) {
+      x = context.canvas.width - radius;
+      moving[index].velocity.x = -moving[index].velocity.x;
+    }
+
+    if (x - radius + (velocity.x * elapsed) / 1000 <= 0 && velocity.x <= 0) {
+      x = radius;
+      moving[index].velocity.x = -moving[index].velocity.x;
+    }
+    //#endregion
+    //#region Gravity
+    // This is DOWNWARDS gravity at the moment
+    if (
+      !(y + (velocity.y * elapsed) / 1000 + radius >= context.canvas.height) &&
+      !(y + (velocity.y * elapsed) / 1000 + radius <= 0) &&
+      !(x + (velocity.x * elapsed) / 1000 + radius >= context.canvas.width) &&
+      !(x + (velocity.x * elapsed) / 1000 + radius <= 0)
+    ) {
+      velocity.y += (gravityPower * elapsed) / 1000;
+    }
+    if (velocity.y + y >= context.canvas.height) {
+      console.log(velocity.y, y, context.canvas.height);
+    }
+
+    ////#endregion
+  });
 }
 
-drawAll();
+function calculateMotion(elapsed) {
+  moving.forEach(function ({ velocity, index }) {
+    moving[index].x += (velocity.x * baseSpeed * elapsed) / 1000;
+    moving[index].y += (velocity.y * baseSpeed * elapsed) / 1000;
+  });
+}
 
+function updateCanvas(timestamp) {
+  if (start === undefined) {
+    start = timestamp;
+  }
+
+  let elapsed = timestamp - start;
+  if (elapsed > 500) {
+    elapsed = 10;
+  }
+  start = timestamp;
+
+  clearCanvas();
+  calculateMotion(elapsed);
+  calculateVelocity(elapsed);
+
+  drawAll();
+  window.requestAnimationFrame(updateCanvas);
+}
+
+window.requestAnimationFrame(updateCanvas);
 // theContext.beginPath();
 // theContext.arc(300, 50, 5, 0, 2 * Math.PI);
 // theContext.fillStyle = "#f7f7f7";
